ece5c3771daa1d4e1821d50beadc5e9f8ff5a6b9
sharada0417 <sharada.kanchana2001@gmail.com>
2025-01-02 23:53:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Studentapp from "./Studentapp"
+
+function App() {
+  return (
+    <>
+      <div>
+       <Studentapp/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Studentapp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Student.css'
+const Studentapp = () => {
+  return (
+    <>
+    <div className='header'>
+    <h1>Student App</h1>
+    </div>
+    <div className='body'>
+    <div className='cardalign'>
+    <div className='card'></div>
+    <div className='card'></div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Studentapp
